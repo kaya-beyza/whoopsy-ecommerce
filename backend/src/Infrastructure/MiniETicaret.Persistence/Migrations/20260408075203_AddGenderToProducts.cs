44d77316199951967c5f573737ae20f6c131e837
Ahmet Bicakci <ahmetbicakci16@gmail.com>
2026-04-08 11:11:52 +0300

    Ürünlere ve Ürünler Tablosuna gender eklendi

---
+++ backend/src/Infrastructure/MiniETicaret.Persistence/Migrations/20260408075203_AddGenderToProducts.cs
@@ -0,0 +1,29 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace MiniETicaret.Persistence.Migrations
+{
+    /// <inheritdoc />
+    public partial class AddGenderToProducts : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AddColumn<int>(
+                name: "Gender",
+                table: "Products",
+                type: "integer",
+                nullable: false,
+                defaultValue: 0);
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropColumn(
+                name: "Gender",
+                table: "Products");
+        }
+    }
+}
