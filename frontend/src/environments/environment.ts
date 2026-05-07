752272f014990b6c84719a3254a7e4b30a593ce8
Ahmet Eren Zembilören <117667017+AhmetEren79@users.noreply.github.com>
2026-05-07 19:02:31 +0300

    Update environment.ts

--- frontend/src/environments/environment.ts
+++ frontend/src/environments/environment.ts
@@ -1,6 +1,6 @@
 export const environment = {
       production: false,
-      apiUrl: 'http://localhost:5277/api',
-      cloudinaryCloudName: 'dog06yvq9',
-      cdnUrl: 'https://res.cloudinary.com/dog06yvq9/image/upload'
+      apiUrl:
+      cloudinaryCloudName:
+      cdnUrl: 
   }
